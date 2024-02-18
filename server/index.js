f15637617b08433a9442248d9ff69ef1b0db7588
Rudger Gravestein <r.gravestein@triple-networks.com>
2024-02-18 10:37:45 +0100

    Do not merge

--- server/index.js
+++ server/index.js
@@ -3,17 +3,63 @@ const _ = require("lodash")
 
 const path = require('path');
 const fs = require('fs');
+const { exit } = require('node:process');
+
 
 async function getContent(src) {
-    const doc = await pdfjs.getDocument(src).promise
-    const page = await doc.getPage(1) // if doc has many pages use doc.numPages to iterate and pass index to doc.getPage
-    return await page.getTextContent()
+    // console.log('src', typeof src);
+    // exit(0);
+
+    const doc = await pdfjs.getDocument({url: src, verbosity: 0}).promise
+
+
+    const docBlob = {
+        metadata: null,
+        pages: []
+    }
+    docBlob.metadata = await doc.getMetadata()
+
+    for (let i = 1; i <= doc.numPages; i++) {
+        const page = await doc.getPage(i)
+        const pageBlob = {}
+        pageBlob.viewport = page.getViewport({ scale: 1.0 })
+        pageBlob.items = []
+
+        const textContent = await page.getTextContent()
+        for (const item of textContent.items) {
+            // Set some convenience properties
+            item.x1 = item.transform[4]
+            item.x2 = item.x1 + item.width
+            item.y1 = item.transform[5]
+            item.y2 = item.y1 + item.height
+
+            pageBlob.items.push(item)
+        }
+
+        docBlob.pages.push(pageBlob)
+    }
+
+    return docBlob;
+
+
+    // if doc has many pages use doc.numPages to iterate and pass index to doc.getPage
+    // const page = await doc.getPage(1)
+
+    // return await page.getTextContent()
 }
 
 async function getItems(src) {
     // Perform pre-processing
-    const content = await getContent(src)
+    const content = await getContent(src);
     // console.log(content.items)
+
+    // docBlob
+
+    console.log(
+    require('util').inspect(content, { depth: null })
+    );
+
+    exit(0);
 
     return content.items
         .filter((item) => item.str.trim().length)
@@ -35,25 +81,28 @@ function processItems(items) {
 
 function handleErrors(error) {
     // handle errors
-    console.log(error)
+    console.error('handleError', error);
 }
 
 const directoryPath = path.join(__dirname, '..', '..', 'pdf', 'notas', '2023');
 
-//passsing directoryPath and callback function
+
 fs.readdir(directoryPath, function (err, files) {
     //handling error
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     }
-    //listing all files using forEach
+
+    // listing all files using forEach
     files.forEach(function (file) {
         // Do whatever you want to do with the file
-        console.log(`${directoryPath}/${file}`);
-
+        // console.log(`${directoryPath}/${file}`);
 
         getItems(`${directoryPath}/${file}`)
             .then(processItems)
             .catch(handleErrors)
+            .finally(() => {
+                exit(0);
+            })
     });
 });
