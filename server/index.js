d0759502f25c6112d27f988ff5fbc1dc20d13583
Rudger Gravestein <r.gravestein@triple-networks.com>
2024-02-18 10:34:50 +0100

    Setup json

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
 const pdfjs = require('pdfjs-dist/es5/build/pdf')
 const _ = require("lodash")
 
+const { exit } = require('node:process');
+
 const path = require('path');
 const fs = require('fs');
 
+
+
 async function getContent(src) {
-    const doc = await pdfjs.getDocument(src).promise
+    const doc = await pdfjs.getDocument({url: src, verbosity: 0}).promise
     const page = await doc.getPage(1) // if doc has many pages use doc.numPages to iterate and pass index to doc.getPage
     return await page.getTextContent()
 }
 
 async function getItems(src) {
     // Perform pre-processing
-    const content = await getContent(src)
-    // console.log(content.items)
+    const content = await getContent(src);
 
     return content.items
         .filter((item) => item.str.trim().length)
@@ -30,12 +33,12 @@ function processItems(items) {
         }, [])
 
     // save json or save csv or write to db
-    console.log(records)
+    console.log(JSON.stringify(records));
 }
 
 function handleErrors(error) {
     // handle errors
-    console.log(error)
+    console.error('Error', error);
 }
 
 const directoryPath = path.join(__dirname, '..', '..', 'pdf', 'notas', '2023');
@@ -49,11 +52,14 @@ fs.readdir(directoryPath, function (err, files) {
     //listing all files using forEach
     files.forEach(function (file) {
         // Do whatever you want to do with the file
-        console.log(`${directoryPath}/${file}`);
-
+        //console.log(`${directoryPath}/${file}`);
 
         getItems(`${directoryPath}/${file}`)
             .then(processItems)
             .catch(handleErrors)
+            .finally(() => {
+                exit(0);
+            })
+
     });
 });
